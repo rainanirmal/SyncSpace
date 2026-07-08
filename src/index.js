d8a106aa70a38b84fbb91c2a6ae50580f9960707
rainanirmal <rainanirmal01@gmail.com>
2026-07-08 22:17:14 +0530

    feat : add MongoDB connection setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/index.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT , () => {
-    console.log(`Server listening on ${PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server listening on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.err("MongoDB connection error : ", err);
+    process.exit(1);
+  });
