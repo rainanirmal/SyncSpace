a9a76292ec9496564fc604a9c6e3c2f26bb1db65
rainanirmal <rainanirmal01@gmail.com>
2026-07-09 13:55:57 +0530

    feat(auth) : add password verification method to user model

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -63,10 +63,14 @@ const UserSchema = new Schema(
 
 UserSchema.pre("save" , async function (next) {
     if(!this.isModified("password")) return next(); 
-    
+
     this.password = await bcrypt.hash(this.password, 10);
     next();
-})
+});
+
+UserSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password , this.password);
+}
 
 const User = mongoose.model("User" , UserSchema);
 
