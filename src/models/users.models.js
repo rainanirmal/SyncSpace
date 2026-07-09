1b644df24ea0fa638419ed3d2bb04207785081e2
rainanirmal <rainanirmal01@gmail.com>
2026-07-09 12:56:30 +0530

    feat : add user model and schema

---
+++ src/models/users.models.js
@@ -0,0 +1,65 @@
+import mongoose , { Schema } from "mongoose";
+
+const UserSchema = new Schema(
+    {
+        avatar: {
+            type: {
+                url: String,
+                localPath: String
+            },
+            default: {
+                url: `https://placehold.co/200x200`,
+                localPath: ""
+            },
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        fullName: {
+            type: String,
+            trim: true
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"]
+        },
+        isEmailVerified: {
+            type: Boolean,
+            default: false
+        },
+        refreshToken: {
+            type: String
+        },
+        forgotPasswordToken: {
+            type: String
+        },
+        forgotPasswordExpiry: {
+            type: Date
+        },
+        emailVerificationToken: {
+            type: String
+        },
+        emailVerificationExpiry: {
+            type: Date
+        }
+    },
+    {
+        timestamps: true,
+    },
+);
+
+const User = mongoose.model("User" , UserSchema);
+
+export { User };
