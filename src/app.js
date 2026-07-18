070446fd205e519ea5d7e5efb793f4067628ef87
rainanirmal <rainanirmal01@gmail.com>
2026-07-18 21:18:35 +0530

    feat : implement user login with cookie-based authentication

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import healthCheckRouter from "./routes/healthcheck.routes.js";
 import authRouter from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended : true}));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 app.use("/api/v1/healthcheck" , healthCheckRouter);
 app.use("/api/v1/auth" , authRouter);
