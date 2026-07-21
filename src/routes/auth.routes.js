20ec5852e9b699b9070087b469baf65ed93076d5
rainanirmal <rainanirmal01@gmail.com>
2026-07-21 23:17:26 +0530

    feat : implement secure user logout

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { registerUser, login } from "../controllers/auth.controllers.js";
+import { registerUser, login, logoutUser } from "../controllers/auth.controllers.js";
 import { validate } from "../middlewares/validator.middlewares.js";
 import { userRegisterValidator, userLoginValidator } from "../validators/index.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(userLoginValidator(), validate, login);
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
