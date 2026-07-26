39f62ca13baf39773e8443d341a12a67e7702b7b
rainanirmal <rainanirmal01@gmail.com>
2026-07-26 18:37:00 +0530

    feat: add change current password controller

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -365,6 +365,33 @@ const resetForgotPassword = asyncHandler(async(req, res) => {
         );
 });
 
+const changeCurrentPassword = asyncHandler(async(req, res) => {
+
+    const { oldPassword, newPassword } = req.body;
+
+    const user = User.findById(req.user?._id);
+
+    const isPasswordValid = await user.isPasswordCorrect(oldPassword);
+
+    if(!isPasswordValid) {
+        throw new ApiError(400, "Invalid old password");
+    }
+
+    user.password = newPassword;
+    
+    await user.save({ validateBeforeSave: false});
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {},
+                "Password changed successfully"
+            )
+        );
+})
+
 export { 
     registerUser, 
     login, 
@@ -375,4 +402,5 @@ export {
     refreshAccessToken,
     forgotPassword,
     resetForgotPassword,
+    changeCurrentPassword,
 };
