60adc0e94de1badd02027e062b90bde274150ff9
rainanirmal <rainanirmal01@gmail.com>
2026-07-26 18:31:38 +0530

    feat: implement reset forgot password controller

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -331,6 +331,40 @@ const forgotPassword = asyncHandler(async(req, res) => {
         );
 });
 
+const resetForgotPassword = asyncHandler(async(req, res) => {
+
+    const { resetToken } = req.params;
+    const { newPassword } = req.body;
+
+    let hashedToken = crypto
+        .createHash("sha256")
+        .update(resetToken)
+        .digest("hex")
+
+    const user = User.findOne({
+        forgotPasswordToken: hashedToken,
+        forgotPasswordExpiry: {$gt: Date.now()}
+    });
+
+    if(!user) {
+        throw new ApiError(489, "Token is invalid or expired");
+    }
+
+    user.forgotPasswordExpiry = undefined;
+    user.forgotPasswordToken = undefined;
+
+    user.password = newPassword;
+    await user.save({ validateBeforeSave: false });
+
+    return res
+        .status(200)
+        .json(
+            200,
+            {},
+            "Password reset suuccessfully"
+        );
+});
+
 export { 
     registerUser, 
     login, 
@@ -339,4 +373,6 @@ export {
     verifyEmail,
     resendEmailVerification,
     refreshAccessToken,
+    forgotPassword,
+    resetForgotPassword,
 };
