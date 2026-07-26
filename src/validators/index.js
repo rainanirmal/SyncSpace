59eff81186e60c8a33c9dfc022cd9e3ad52790b8
rainanirmal <rainanirmal01@gmail.com>
2026-07-26 18:52:12 +0530

    feat: add validators for forgot, reset and change password

--- src/validators/index.js
+++ src/validators/index.js
@@ -39,7 +39,39 @@ const userLoginValidator = () => {
     ];
 }
 
+const userChangeCurrentPasswordValidator = () => {
+    return [
+        body("oldPassword")
+            .notEmpty()
+            .withMessage("Old password is required"),
+        body("newPassword")
+            .notEmpty()
+            .withMessage("New password is required")
+    ];
+}
+
+const userForgotPasswordValidator = () => {
+    return [
+        body("email")
+            .notEmpty()
+            .withMessage("Email is required")
+            .isEmail()
+            .withMessage("Email is invalid")
+    ];
+}
+
+const userResetForgotPasswordValidator = () => {
+    return [
+        body("newPasswod")
+            .notEmpty()
+            .withMessage("Password id required")
+    ];
+}
+
 export {
     userRegisterValidator,
     userLoginValidator,
+    userChangeCurrentPasswordValidator,
+    userForgotPasswordValidator,
+    userResetForgotPasswordValidator,
 }
