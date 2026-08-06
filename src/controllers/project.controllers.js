67cef7bac64168b2a68444cd4d37db3598d3d09f
rainanirmal <rainanirmal01@gmail.com>
2026-08-06 23:49:07 +0530

    feat: implement project listing with aggregation pipeline

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -8,7 +8,68 @@ import mongoose, { mongo } from "mongoose";
 import { UserRolesEnum } from "../utils/constants.js";
 
 const getProjects = asyncHandler(async(req, res) => {
-    //test
+    
+    const project = await ProjectMember.aggregate(
+        [
+            {
+                $match: {
+                    user: new mongoose.Types.ObjectId(req.user._id)
+                }
+            },
+            {
+                $lookup: {
+                    from: "projects",
+                    localField: "project",
+                    foreignField: "_id",
+                    as: "projects",
+                    pipeline: [
+                        {
+                            $lookup: {
+                                from: "projectmembers",
+                                localField: "_id",
+                                foreignField: "project",
+                                as: "projectmembers"
+                            }
+                        },
+                        {
+                            $addFields: {
+                                members: {
+                                    $size: "$projectmembers",
+                                }
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                $unwind: "$projects"
+            },
+            {
+                $project: {
+                    project: {
+                        _id: 1,
+                        name: 1,
+                        description: 1,
+                        members: 1,
+                        createdBy: 1,
+                        createdAt: 1
+                    },
+                    role: 1,
+                    _id: 0
+                }
+            }
+        ]
+    );
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                project,
+                "Project fetched successfully !"
+            )
+        );
 });
 
 const getProjectById = asyncHandler(async(req, res) => {
