c5be80c52ab96d447718c7eadb75adf9124592c7
rainanirmal <rainanirmal01@gmail.com>
2026-08-07 10:14:42 +0530

    feat: implement get project by ID controller

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -73,7 +73,24 @@ const getProjects = asyncHandler(async(req, res) => {
 });
 
 const getProjectById = asyncHandler(async(req, res) => {
-    //test
+    
+    const { projectId } = req.params;
+
+    const project = await Project.findById(projectId);
+
+    if(!project) {
+        throw new ApiError(404, "Project not found !");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                project,
+                "Project fetched successfully !"
+            )
+        );
 });
 
 const createProject = asyncHandler(async(req, res) => {
