d2891c332a9212ae32b642e4f37056ec567ccd01
rainanirmal <rainanirmal01@gmail.com>
2026-08-07 11:53:41 +0530

    feat: implement get project members with aggregation pipeline

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -206,7 +206,69 @@ const addMembersToProject = asyncHandler(async(req, res) => {
 });
 
 const getProjectMembers = asyncHandler(async(req, res) => {
-    //test
+    
+    const { projectId } = req.params;
+
+    const project = await Project.findById(projectId);
+
+    if(!project) {
+        throw new  ApiError(404, "Project not found !");
+    }
+
+    const projectMemebers = await ProjectMember.aggregate(
+        [
+            {
+                $match: {
+                    project: new mongoose.Types.ObjectId(projectId)
+                },
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "user",
+                    foreignField: "_id",
+                    as: "user",
+                    pipeline: [
+                        {
+                            $project: {
+                                _id: 1,
+                                username: 1,
+                                fullName: 1,
+                                avatar: 1
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                $addFields: {
+                    user: {
+                        $arrayElemAt: ["$user", 0]
+                    }
+                }
+            },
+            {
+                $project: {
+                    project: 1,
+                    user: 1,
+                    role: 1,
+                    createdAt: 1,
+                    updatedAt: 1,
+                    _id: 0
+                }
+            }
+        ]
+    );
+
+    return res  
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                projectMemebers,
+                "Project members fetched !"
+            )
+        );
 });
 
 const updateMemberRole = asyncHandler(async(req, res) => {
