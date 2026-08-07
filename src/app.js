5339e3d045d33ec07bd433fa095554c13b769efa
rainanirmal <rainanirmal01@gmail.com>
2026-08-07 15:06:49 +0530

    feat: register project routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import healthCheckRouter from "./routes/healthcheck.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
+import projectRouter from "./routes/project.routes.js";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(cookieParser());
 
 app.use("/api/v1/healthcheck" , healthCheckRouter);
 app.use("/api/v1/auth" , authRouter);
+app.use("/api/v1/projects", projectRouter); 
 
 app.get("/" , (req, res) => {
     res.send("Hello from raina !");
